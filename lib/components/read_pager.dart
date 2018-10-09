91ab0a239545e7bcc0579837e4d5617d02ac02e1
NightFarmer <nightfarmer@163.com>
2018-10-09 16:00:45 +0800

    调整分页逻辑 调整章节文本为自绘

--- lib/components/read_pager.dart
+++ lib/components/read_pager.dart
@@ -7,6 +7,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
 import 'package:seek_book/components/read_pager_item.dart';
+import 'package:seek_book/components/text_canvas.dart';
 import 'package:seek_book/utils/screen_adaptation.dart';
 import 'package:seek_book/globals.dart' as Globals;
 
@@ -40,6 +41,7 @@ class _ReadPagerState extends State<ReadPager> {
   TextStyle textStyle;
   double ReadTextWidth;
   double ReadTextHeight;
+  double LineHeight;
 
   PageController pageController;
 
@@ -53,6 +55,9 @@ class _ReadPagerState extends State<ReadPager> {
     ReadTextWidth = ScreenAdaptation.screenWidth - dp(32);
     ReadTextHeight =
         ScreenAdaptation.screenHeight - dp(35) - dp(44); //减去头部章节名称高度，减去底部页码高度
+    LineHeight = dp(27);
+    var lineNum = (ReadTextHeight / LineHeight).floor();
+    LineHeight = (ReadTextHeight/lineNum).floorToDouble();
     textStyle = new TextStyle(
       height: 1.2,
       fontSize: dp(17),
@@ -99,6 +104,10 @@ class _ReadPagerState extends State<ReadPager> {
       return;
     }
     var url = chapterList[chapterIndex]['url'];
+    var exist = chapterTextMap[url];
+    if (exist != null) {
+      return;
+    }
 
     var database = Globals.database;
     List<Map> existData =
@@ -139,7 +148,14 @@ class _ReadPagerState extends State<ReadPager> {
     if (chapterTextMap[url] == null) {
       return [0];
     }
-    var pageEndIndexList = parseChapterPager(chapterTextMap[url]);
+//    var pageEndIndexList = parseChapterPager(chapterTextMap[url]);
+    var pageEndIndexList = ChapterTextPainter.calcPagerData(
+      chapterTextMap[url],
+      ReadTextWidth,
+      ReadTextHeight,
+      textStyle,
+      LineHeight,
+    );
     chapterPagerDataMap[url] = pageEndIndexList;
     print(pageEndIndexList);
     print("页数 ${pageEndIndexList.length}");
@@ -247,89 +263,6 @@ class _ReadPagerState extends State<ReadPager> {
     );
   }
 
-  //测量章节分页逻辑=============⬇=======⬇==========⬇️=============⬇⬇️
-
-  // 解析一个章节所有分页每页最后字符的index列表
-  List<int> parseChapterPager(String content) {
-    List<int> pageEndPointList = List();
-    do {
-      var contentNeedToParse = content;
-      var prePageEnd = 0;
-      if (pageEndPointList.length > 0) {
-        prePageEnd = pageEndPointList[pageEndPointList.length - 1];
-        contentNeedToParse = content.substring(
-          prePageEnd,
-          min(prePageEnd + pageEndPointList[0] * 2, content.length),
-        );
-//        contentNeedToParse = content.substring(prePageEnd);
-      }
-      pageEndPointList.add(prePageEnd + getOnePageEnd(contentNeedToParse));
-    } while (pageEndPointList.length == 0 ||
-        pageEndPointList[pageEndPointList.length - 1] != content.length);
-
-    return pageEndPointList;
-  }
-
-  /// 传入需要计算分页的文本，返回第一页最后一个字符的index
-  int getOnePageEnd(String text) {
-    if (layout(text)) {
-//      return false;
-      return text.length;
-    }
-
-    int start = 0;
-    int end = text.length;
-    int mid = (end + start) ~/ 2;
-
-    var time = 0;
-    // 最多循环20次
-    for (int i = 0; i < 20; i++) {
-      time++;
-      if (layout(text.substring(0, mid))) {
-        if (mid <= start || mid >= end) break;
-        // 未越界
-        start = mid;
-        mid = (start + end) ~/ 2;
-      } else {
-        // 越界
-        end = mid;
-        mid = (start + end) ~/ 2;
-      }
-    }
-//    print('循环次数 ${time}');
-    return mid;
-  }
-
-  /// 计算待绘制文本
-  /// 未超出边界返回true
-  /// 超出边界返回false
-  bool layout(String text) {
-    text = text ?? '';
-    var textPainter = TextPainter(textDirection: TextDirection.ltr);
-    textPainter
-      ..text = getTextSpan(text)
-      ..layout(maxWidth: ReadTextWidth);
-    return !didExceed(textPainter);
-  }
-
-  /// 是否超出边界
-  bool didExceed(textPainter) {
-    return textPainter.didExceedMaxLines ||
-        textPainter.size.height > ReadTextHeight;
-  }
-
-  /// 获取带样式的文本对象
-  TextSpan getTextSpan(String text) {
-//    if (text.startsWith('\n')) {
-//      text = text.substring(1);
-//    }
-    // 判定时，移除可能是本页文本的最后一个换行符，避免造成超过一页
-    if (text.endsWith('\n')) {
-      text = text.substring(0, text.length - 1);
-    }
-    return new TextSpan(text: text, style: textStyle);
-  }
-
   Widget buildPage(int index) {
 //    print("MM");
     var pageIndex = currentPageIndex + (index - initScrollIndex);
@@ -384,9 +317,15 @@ class _ReadPagerState extends State<ReadPager> {
     }
 
     return ReadPagerItem(
-      text: new Text(
-        text,
-        style: textStyle,
+//      text: new Text(
+//        text,
+//        style: textStyle,
+//      ),
+      text: new TextCanvas(
+        text: text,
+        width: ReadTextWidth,
+        height: ReadTextHeight,
+        lineHeight: LineHeight,
       ),
       title: title,
       pageLabel: pageLabel,
---
+++ lib/components/text_canvas.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:seek_book/utils/screen_adaptation.dart';
+
+class ChapterTextPainter extends CustomPainter {
+  static List<int> calcPagerData(text, width, height, textStyle, lineHeight) {
+    List<int> result = [];
+
+    var allChart = text.split('');
+
+    double charHeight = lineHeight;
+
+    double xOffset = 0.0;
+    int yCharCount = 0;
+    int lineCount = 0;
+    for (int i = 0; i < allChart.length; i++) {
+      var char = allChart[i];
+      TextSpan span = new TextSpan(style: textStyle, text: char);
+      TextPainter tp = new TextPainter(
+          text: span,
+          textAlign: TextAlign.left,
+          textDirection: TextDirection.ltr);
+      tp.layout();
+
+      if (char == '\n' || (xOffset + tp.size.width) > width) {
+        xOffset = 0.0;
+        yCharCount++;
+      }
+      if ((yCharCount + 1) * charHeight > height && char != '\n') {
+        yCharCount = 0;
+        result.add(i);
+      }
+      xOffset += 1 * tp.size.width;
+    }
+    print("新算法算出来的分页页码");
+    print(result);
+    return result;
+  }
+
+  final String text;
+
+  final double width;
+  final double height;
+  final double lineHeight;
+
+  ChapterTextPainter({this.text, this.width, this.height, this.lineHeight});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //
+//    Paint paint = new Paint() //设置笔的属性
+//      ..color = Colors.blue[200]
+//      ..strokeCap = StrokeCap.round
+//      ..isAntiAlias = true
+//      ..strokeWidth = 12.0
+//
+//      ..strokeJoin = StrokeJoin.bevel;
+
+//    canvas.draw
+
+    var textStyle = new TextStyle(
+      height: 1.2,
+      fontSize: dp(17),
+      letterSpacing: dp(1),
+      color: Color(0xff383635),
+//        fontFamily: 'ReadFont',
+    );
+    var allChart = text.split('');
+
+//    var charWidth = 18;
+//    var charHeight = 27;
+    var charLineCount = 20;
+
+    double xOffset = 0.0;
+    int xCharCount = 0;
+    int yCharCount = 0;
+//    double yOffset = 0.0;
+    int lineCount = 0;
+    for (int i = 0; i < allChart.length; i++) {
+      var char = allChart[i];
+      TextSpan span = new TextSpan(style: textStyle, text: char);
+      TextPainter tp = new TextPainter(
+          text: span,
+          textAlign: TextAlign.left,
+          textDirection: TextDirection.ltr);
+      tp.layout();
+
+//      if (char == '\n' || (xCharCount + 1) > charLineCount) {
+      if (char == '\n' || (xOffset + tp.size.width) > width) {
+        xOffset = 0.0;
+        xCharCount = 0;
+        yCharCount++;
+//        if (char == '\n') {//多绘制一个换行符 并不要紧
+//          continue;
+//        }
+      }
+
+//      print("$char\t${tp.size}");
+      var yOffset = yCharCount * lineHeight +
+          (lineHeight - tp.size.height) / 2;
+      tp.paint(canvas, new Offset(xOffset, yOffset));
+
+      xCharCount++;
+      xOffset += 1 * tp.size.width;
+    }
+  }
+
+//  charWidth(char) {
+//    var indexOf = littleChars.indexOf(char);
+////    print("$indexOf\t$char");
+//    return indexOf >= 0 ? 13.0 : 18.0;
+//  }
+
+//  charTopOffset(char) {
+//    var indexOf = numbers.indexOf(char);
+//    return indexOf >= 0 ? 3.0 : 0;
+//  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class TextCanvas extends StatelessWidget {
+  final String text;
+  final double width;
+  final double height;
+  final double lineHeight;
+
+  TextCanvas({Key key, this.text, this.width, this.height, this.lineHeight})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: new ChapterTextPainter(
+        text: text,
+        width: width,
+        height: height,
+        lineHeight: lineHeight,
+      ),
+    );
+  }
+}
